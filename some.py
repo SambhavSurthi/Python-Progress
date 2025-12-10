9cce3a134642117febc0ee55df518b4c2704c75e
SambhavSurthi <sambhavsurthi14@gmail.com>
2025-12-10 21:41:15 +0530

    10 Dec - packages

---
+++ some.py
@@ -0,0 +1,7 @@
+# from mypackage.module1 import addition
+# from mypackage.module2 import multiplication
+# print(addition.add(2,5))
+# print(multiplication.mul(2,5))
+
+from mypackage.module1 import add
+print(add(2,5))
