0c74577d793a2a01b612e655320ceebf1b900db7
Sambhav Surthi <sambhavsurthi14@gmail.com>
2026-01-27 21:53:38 +0530

    Update some.py

--- 0.Test/some.py
+++ 0.Test/some.py
@@ -1,5 +1,5 @@
 # from mypackage.module1 import addition
-# from mypackage.module2 import multiplication
+# from mypackage.module2 import multiplications
 # print(addition.add(2,5))
 # print(multiplication.mul(2,5))
 
